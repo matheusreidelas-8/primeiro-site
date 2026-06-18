a2955f1514f0055ff8c20e056d8e921923c21928
matheusreidelas-8 <mendes.lara.matheus@escola.pr.gov.br>
2026-06-18 08:54:21 -0300

    hj eu fiz tudo q eu podia

---
+++ script.js
@@ -0,0 +1,22 @@
+const botao = document.querySelector("button")
+botao.addEventListener('click', curtir)
+
+function curtir() {
+    let curtidas = document.querySelector("span")
+    curtidas.textContent++;
+}
+
+
+// BOTÃO DO TAMANHO
+const tamanho = document.getElementById("tamanho")
+tamanho.addEventListener("click", aumentarFonte)
+let fonte = 16;
+
+//FUNÇÃO
+function aumentarFonte(){
+    fonte++;
+    document.body.style.fontSize = fonte + "px";
+}
+
+// BOTÃO DO CONTRASTE
+// FUNÇÃO DO CONTRASTE
